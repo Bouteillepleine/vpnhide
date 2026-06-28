d1312e1ead1336c8e35250944701dc03678b3d46
okhsunrog <me@okhsunrog.dev>
2026-06-28 23:21:30 +0300

    fix: repair native module packaging

--- kmod/module/customize.sh
+++ kmod/module/customize.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
+# shellcheck disable=SC2034
 SKIPUNZIP=0
 MOD_VER="$(grep '^version=' "$MODPATH/module.prop" | cut -d= -f2)"
 ui_print "- VPN Hide (kernel) ${MOD_VER:-unknown}"
@@ -10,6 +11,9 @@ PERSIST_DIR="/data/adb/vpnhide_kmod"
 mkdir -p "$PERSIST_DIR"
 set_perm "$PERSIST_DIR" 0 0 0755
 
+set_perm "$MODPATH/activator" 0 0 0755
+set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
+set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm "$MODPATH/vpnhide_kmod.ko" 0 0 0644
 set_perm "$MODPATH/uninstall.sh" 0 0 0755
 
--- zygisk/module/customize.sh
+++ zygisk/module/customize.sh
@@ -3,6 +3,7 @@
 # have been extracted to $MODPATH. Anything we write to $MODPATH persists
 # into /data/adb/modules/<id>/ after the installer finishes.
 
+# shellcheck disable=SC2034
 SKIPUNZIP=0
 MOD_VER="$(grep '^version=' "$MODPATH/module.prop" | cut -d= -f2)"
 ui_print "- VPN Hide (Zygisk) ${MOD_VER:-unknown}"
@@ -10,6 +11,8 @@ ui_print "- Installing to $MODPATH"
 
 # Make the native library readable/executable by zygote
 set_perm "$MODPATH/zygisk/arm64-v8a.so" 0 0 0755
+set_perm "$MODPATH/activator" 0 0 0755
+set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm "$MODPATH/uninstall.sh" 0 0 0755
 
 # ----------------------------------------------------------------------
